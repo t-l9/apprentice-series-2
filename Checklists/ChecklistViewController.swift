d625f13b197c8c42bfd57d5fa053513168110e3b
Tim Lauter <timlauter@0-0.local>
2016-11-05 00:28:39 -0400

    added TableViewMethod

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -19,7 +19,17 @@ class ChecklistViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        return cell
+    }
 
 }
-
